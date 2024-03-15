83e94f37ea733f8995eed24c1b15080350ebdd00
LittlePechya <vladkaaa47@gmail.com>
2024-03-15 12:16:57 +0700

    setup for placeholder release

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // Open popup
-document.getElementById("login-button").addEventListener("click", function(){
+document.getElementById("open-login-form-button").addEventListener("click", function(){
     var popup = document.querySelector(".popup");
     popup.classList.add("popup__active");
 })
@@ -9,3 +9,38 @@ document.getElementById("close-button").addEventListener("click", function(){
     var popup = document.querySelector(".popup");
     popup.classList.remove("popup__active");
 })
+
+const loginUrl = 'http://10.22.202.183:8000/api/v1/auth/login';
+
+function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  console.log("click");
+
+  const data = {
+    email: email,
+    password: password
+  };
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch(loginUrl, options)
+    .then(response => response.json())
+    .then(data => {
+      // Обработка успешного ответа от сервера
+      console.log(data);
+    })
+    .catch(error => {
+      // Обработка ошибки
+      console.error('Ошибка:', error);
+    });
+}
+
+const loginButton = document.getElementById('login-button');
+loginButton.addEventListener('click', login);
